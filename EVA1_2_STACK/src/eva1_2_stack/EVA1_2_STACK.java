81286d0c93ff94c12fb3376d50ab1cdba91962be
EvelynAnetteDuarteLopez <L24550849@chihuahua2.tecnm.mx>
2026-01-29 21:27:23 -0600

    Add files via upload

---
+++ EVA1_2_STACK/src/eva1_2_stack/EVA1_2_STACK.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_stack;
+
+/**
+ *
+ * @author evelyn Anette Duarte Lopez  29/01/2026
+ */
+public class EVA1_2_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       A();
+    }
+    
+    public static void A(){
+       B();
+    }
+    
+    public static void B(){
+        int i=5;
+        double b=10.5;
+    }
+}
