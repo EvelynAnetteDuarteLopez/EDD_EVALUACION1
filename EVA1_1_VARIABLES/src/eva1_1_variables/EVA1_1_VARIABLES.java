92b0674dc946c9512f17a609e547dbea5a969ed2
EvelynAnetteDuarteLopez <L24550849@chihuahua2.tecnm.mx>
2026-01-29 21:25:51 -0600

    Add files via upload

---
+++ EVA1_1_VARIABLES/src/eva1_1_variables/EVA1_1_VARIABLES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_variables;
+
+/**
+ *
+ * @author Evelyn Anette Duarte Lopez 28/01/2026
+ */
+public class EVA1_1_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VARIABLES LOCALES: 
+        //cualquier variable declarada en un metodo
+        int x= 100; // 4 bytes
+        char caracter = 'c'; //2 bytes
+        
+        //VARIABLES DINAMICAS: --->HEAP
+        //OBJETOS
+        persona per = new persona();  
+}   
+}
+class persona{
+    
+}
