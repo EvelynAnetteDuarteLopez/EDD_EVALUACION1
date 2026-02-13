4557ed96e77d610fc079f47b457e610bafa27aed
EvelynAnetteDuarteLopez <L24550849@chihuahua2.tecnm.mx>
2026-02-13 14:35:54 -0600

    Add files via upload

---
+++ EVA1_14_CUBO/src/eva1_14_cubo/EVA1_14_CUBO.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_14_cubo;
+
+/**
+ *
+ * @author eveli
+ */
+public class EVA1_14_CUBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[][][] cubo = new int[3][5][2];
+        for(int i=0; i < cubo.length; i++){//primer dimension
+            for(int j =0; j <cubo[i].length; j++){
+                for(int k=0; k<cubo[i][j].length; k++){
+                    cubo[i][j][k]= (int)(Math.random() * 100);
+                    
+                    
+                }
+            }
+        }
+    }
+    
+}
